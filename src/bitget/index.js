ef90227fafd4f1ee04bd15ad9097ce3f90c2de3a
lidd95 <lidd95@chinaunicom.cn>
2024-09-20 13:25:15 +0800

    111

--- src/bitget/index.js
+++ src/bitget/index.js
@@ -1,35 +1,35 @@
-import {SocksProxyAgent} from 'socks-proxy-agent';  
-import fetch from 'node-fetch';  
-const proxy = 'socks5h://127.0.0.1:10808';  
-const agent = new SocksProxyAgent(proxy);  
+import { SocksProxyAgent } from 'socks-proxy-agent'
+import fetch from 'node-fetch'
+const proxy = 'socks5h://127.0.0.1:10808'
+const agent = new SocksProxyAgent(proxy)
 
 export async function getBitgetReply(content) {
-    // 拼接字符串"USDT"  
-    content = content.toUpperCase()+ "USDT"; 
-    console.log('🚀🚀🚀 / content', content)
-    try {  
-      const response = await fetch('https://api.bitget.com/api/v2/spot/market/tickers?symbol='+content, {  
-        agent,  
-        timeout: 5000  
-      })
-      const data = await response.json();  
+  // 拼接字符串"USDT"
+  content = content.toUpperCase() + 'USDT'
+  console.log('🚀🚀🚀 / content', content)
+  try {
+    // const response = await fetch('https://api.bitget.com/api/v2/spot/market/tickers?symbol='+content, {
+    //   agent,
+    //   timeout: 5000
+    // })
+    const response = await fetch('https://api.bitget.com/api/v2/spot/market/tickers?symbol=' + content)
+    const data = await response.json()
 
-        // 检查data属性是否非空  
-      if (!data || !data.data || data.data.length === 0) {  
-        return `未查到币种: ${content}`; 
-      }   
-      // 假设我们只对第一个元素感兴趣  
-      const tickerData = data.data[0];  
-    
-      // 提取symbol和lastPr  
-      const { symbol: tickerSymbol, lastPr: tickerLastPr , open: tickeropen } = tickerData;  
-      console.log(`${tickerSymbol}, Last Price: ${tickerLastPr}`)
+    // 检查data属性是否非空
+    if (!data || !data.data || data.data.length === 0) {
+      return `未查到币种: ${content}`
+    }
+    // 假设我们只对第一个元素感兴趣
+    const tickerData = data.data[0]
 
+    // 提取symbol和lastPr
+    const { symbol: tickerSymbol, lastPr: tickerLastPr, open: tickeropen } = tickerData
+    console.log(`${tickerSymbol}, Last Price: ${tickerLastPr}`)
 
-      // 构造并返回所需信息  
-      return `${tickerSymbol}, Last Price: ${tickerLastPr}(${((tickerLastPr/tickeropen-1)*100).toFixed(4)}%)`;  
-    } catch (error) {  
-      console.error('请求失败:', error); // 处理请求错误  
-      return `查寻币种: ${content}失败`;  
-    }  
+    // 构造并返回所需信息
+    return `${tickerSymbol}, Last Price: ${tickerLastPr}(${((tickerLastPr / tickeropen - 1) * 100).toFixed(4)}%)`
+  } catch (error) {
+    console.error('请求失败:', error) // 处理请求错误
+    return `查寻币种: ${content}失败`
   }
+}
