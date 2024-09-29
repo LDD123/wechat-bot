c00e741ea6d5c9b1407e3d624c48ff68bd88c7f1
lidd95 <1135835030@qq.com>
2024-09-29 23:33:19 +0800

    11111

--- src/gateIo/index.js
+++ src/gateIo/index.js
@@ -13,16 +13,28 @@ export async function getGateIoReply(content) {
     // })
     const response = await fetch(`https://api.gateio.ws/api/v4/spot/tickers?currency_pair=${content}_USDT&timezone=all`)
     const data = await response.json()
-    const gateObj= data[0]
-    // 检查data属性是否非空
-    if (!gateObj.hasOwnProperty('currency_pair') ) {
+    if (Array.isArray(data)) {  
+      const gateObj= data[0]
+      const {currency_pair,last,change_percentage} = gateObj
+      // 构造并返回所需信息
+      return `${currency_pair}, Last Price: $${last}(${change_percentage}%)`
+    } else if (typeof data === 'object' && data !== null) {  
+      if(!data.hasOwnProperty('currency_pair')){
+        //调用bitget交易所查询币种
+        const bitgetResponse = await getBitgetReply(content)
+        return bitgetResponse
+      }else{
+        const {currency_pair,last,change_percentage} = data
+        // 构造并返回所需信息
+        return `${currency_pair}, Last Price: $${last}(${change_percentage}%)`
+      }
+       
+    }else{
       //调用bitget交易所查询币种
       const bitgetResponse = await getBitgetReply(content)
       return bitgetResponse
     }
-    const {currency_pair,last,change_percentage} = gateObj
-    // 构造并返回所需信息
-    return `${currency_pair}, Last Price: $${last}(${change_percentage}%)`
+    
   } catch (error) {
     console.error('请求失败:', error) // 处理请求错误
     return `查寻币种: ${content}失败`
